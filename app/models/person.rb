9efcd1f4caeca97c70037426c8184aef22b1fa54
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-06-25 20:05:56 -0300

    config uploader for model person and validation name by person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,5 @@
 class Person < ApplicationRecord
+  mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.   # Fala ao rails para usar esse uploader para esse modell
+  validates :name, presence: true # Make sure the owner's name is present.   # Verifica se o nome da pessoa existe
+
 end
