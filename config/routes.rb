cb2ceff4eb0361d769eabe260f717e054f45ef2b
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-06-25 20:00:41 -0300

    criado model Person com index new destroy..

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'person/index'
+  get 'person/new'
+  get 'person/create'
+  get 'person/show'
+  get 'person/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/controllers/person_controller_test.rb
+++ test/controllers/person_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class PersonControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get person_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get person_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get person_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get person_show_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get person_destroy_url
+    assert_response :success
+  end
 end
