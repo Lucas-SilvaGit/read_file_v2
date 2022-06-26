c8c0ba6086fd8eec4df9f2f608658a3de50aae0f
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-06-26 11:20:45 -0300

    alteration resources person

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'person/index'
-  get 'person/new'
-  get 'person/create'
-  get 'person/show'
-  get 'person/destroy'
+  resources :person
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
